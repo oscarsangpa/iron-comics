a92234ea82f4d30518f1ff067a41280a4f1630b7
Oscar Sangrador <oscarsangrador@gmail.com>
2022-02-21 13:01:39 +0100

    added bcrypt

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const EMAIL_PATTERN = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 /* La contraseña debe tener entre 8 y 15 caracteres, al menos un dígito, una minúscula, una mayúscula y caracter especial */
@@ -22,4 +23,28 @@ const UserSchema = new mongoose.Schema({
     required: 'password is required',
     match: [PASSWORD_PATTERN, 'password needs between 8 to 15 characters which contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character'],
   }
+});
+
+
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.hash(user.password, SALT_ROUNDS)
+      .then((hash) => {
+        user.password = hash
+        next()
+      })
+      .catch(err => next(err))
+  } else {
+    next()
+  }
 })
+
+userSchema.methods.checkPassword = function(password) {
+  return bcrypt.compare(password, this.password)
+}
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
