674d66391c28aeb0bdabfd93b44b3de6903306b8
Oscar Sangrador <oscarsangrador@gmail.com>
2022-02-21 12:54:33 +0100

    added user model and stronger password regex

---
+++ models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const EMAIL_PATTERN = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+/* La contraseña debe tener entre 8 y 15 caracteres, al menos un dígito, una minúscula, una mayúscula y caracter especial */
+const PASSWORD_PATTERN = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+const SALT_ROUNDS = 10;
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: 'name is required',
+    minLength: [4, 'name needs at least 4 chars']
+  },
+  email: {
+    type: String,
+    required: 'email is required',
+    match: [EMAIL_PATTERN, 'email is not valid'],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: 'password is required',
+    match: [PASSWORD_PATTERN, 'password needs between 8 to 15 characters which contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character'],
+  }
+})
