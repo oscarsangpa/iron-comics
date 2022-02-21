24cf12911805dbbe576e6a71279fe42d15ad433c
Oscar Sangrador <oscarsangrador@gmail.com>
2022-02-21 12:48:19 +0100

    first steps (databases config)

---
+++ app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const createError = require('http-errors');
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+
+require('./config/db.config')
+
+const app = express();
+
+
+
+const port = Number(process.env.PORT || 3000);
+
+app.listen(port, () => {
+  console.log(`App listen on port ${port}`)
+});
