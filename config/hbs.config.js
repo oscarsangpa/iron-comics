697c69a094b341bf6b4a2fdd889caef66b4a1d2b
Oscar Sangrador <oscarsangrador@gmail.com>
2022-03-10 21:38:05 +0100

    clean files

--- config/hbs.config.js
+++ config/hbs.config.js
@@ -1,11 +1,24 @@
 const hbs = require('hbs');
 
 hbs.registerPartials('./views/partials');
+const notImage ="http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available";
 
-hbs.registerHelper('notImage', function (options) {
+
+
+hbs.registerHelper('imageNotFound', function (options) {
   const { comic, thumbnail } = options.hash;
 
-  if (comic.thumbnail.path.includes("http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available")) {
+  if (comic.thumbnail.path === notImage) {
+    return options.fn(this);
+  } else {
+    return options.inverse(this);
+  }
+})
+
+hbs.registerHelper('restaurantHasCategory', function (options) {
+  const { restaurant, category } = options.hash;
+
+  if (restaurant && restaurant.categories.includes(category)) {
     return options.fn(this);
   } else {
     return options.inverse(this);
