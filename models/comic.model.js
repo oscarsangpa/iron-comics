2983ea44246bbafe7bd59299aa929adb85b0ae48
AlejandroGC1990 <alejandro.garcia.carmona@gmail.com>
2022-03-12 00:33:56 +0100

    comics.json

--- models/comic.model.js
+++ models/comic.model.js
@@ -1,18 +1,34 @@
 const mongoose = require("mongoose");
 
 const ComicSchema = new mongoose.Schema({
-    title: { type: String, required: "Title is required" },
-    author: { type: String, required: "Author is required" },
-    cover: {
-      type: String,
-      default:
+    title: { 
+        type: String, 
+        required: "Title is required" },
+    author: { 
+        type: String, 
+        required: "Author is required" },
+    cartoonist: {
+        type: String,
+        require: "Cartoonist is requerid" }, 
+    image: {
+        type: String,
+        default:
         "https://res.cloudinary.com/ddu4a2pzu/image/upload/v1646488530/get-lit/no-cover_atepnu.png",
     },
-    year: { type: Number },
-    pages: { type: Number },
-    genre: { type: String },
-    synopsis: { type: String },
-    reviews: { type: mongoose.Schema.Types.ObjectId, ref: "Comment" },
-  },
-  { timestamps: true, toJSON: { virtuals: true } }
+    year: {
+        type: Number },
+    categories: { 
+        type: String },
+    // reviews: { 
+    //     type: mongoose.Schema.Types.ObjectId, 
+    //     ref: "Comment" },
+},
+  
+{ timestamps: true, 
+    toJSON: { 
+        virtuals: true } }
 );
+
+const Comic = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
