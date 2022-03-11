d8dff2cf9699bafb9f6ced7cb2885a00ee7ef879
AlejandroGC1990 <alejandro.garcia.carmona@gmail.com>
2022-03-11 23:30:48 +0100

    comic model

--- models/comic.model.js
+++ models/comic.model.js
@@ -1,3 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const ComicSchema = new mongoose.Schema({})
+const ComicSchema = new mongoose.Schema({
+    title: { type: String, required: "Title is required" },
+    author: { type: String, required: "Author is required" },
+    cover: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/ddu4a2pzu/image/upload/v1646488530/get-lit/no-cover_atepnu.png",
+    },
+    year: { type: Number },
+    pages: { type: Number },
+    genre: { type: String },
+    synopsis: { type: String },
+    reviews: { type: mongoose.Schema.Types.ObjectId, ref: "Comment" },
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+);
