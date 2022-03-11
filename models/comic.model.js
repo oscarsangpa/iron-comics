bc150d6eb606beb409ffd8b70d4aa15a2930c54a
Oscar Sangrador <oscarsangrador@gmail.com>
2022-03-12 00:45:51 +0100

    confirm

--- models/comic.model.js
+++ models/comic.model.js
@@ -1,18 +1,33 @@
 const mongoose = require("mongoose");
 
 const ComicSchema = new mongoose.Schema({
-    title: { type: String, required: "Title is required" },
-    author: { type: String, required: "Author is required" },
-    cover: {
-      type: String,
-      default:
-        "https://res.cloudinary.com/ddu4a2pzu/image/upload/v1646488530/get-lit/no-cover_atepnu.png",
+    title: { 
+      type: String, 
+      required: "Title is required" 
     },
-    year: { type: Number },
-    pages: { type: Number },
-    genre: { type: String },
-    synopsis: { type: String },
-    reviews: { type: mongoose.Schema.Types.ObjectId, ref: "Comment" },
+    author: { 
+      type: String,
+      required: "Author is required" 
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    categories: { 
+      type: String 
+    },
+    description: { 
+      type: String 
+    },
+    reviews: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: "Comment" 
+    },
   },
-  { timestamps: true, toJSON: { virtuals: true } }
+  { timestamps: true,
+     toJSON: { 
+       virtuals: true } }
 );
+
+const Comic = mongoose.model('Comic', ComicSchema);
+module.exports = Comic;
